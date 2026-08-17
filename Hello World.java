0c6bdccbe63b4714841c5d7d1b9ec27a56b7792b
Kondoju Gayathri <gayathrikondoju98@gmail.com>
2026-08-17 14:51:55 +0530

    Update Hello World.java

--- Hello World.java
+++ Hello World.java
@@ -1,6 +1,6 @@
- public class Hello World{
-  public Static void main(String args[])
+ public class HelloWorld{
+  public static void main(String args[])
     {
-  System.out.println("hello.world");
+  System.out.println("hello world");
     }
   }
